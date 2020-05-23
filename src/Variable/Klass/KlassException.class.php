e2177176f7bc08c548d77ca229b905c594d0b0b8
kouhei.takemoto <take64@citrus.tk>
2020-05-23 13:29:21 +0900

    KlassException生成

---
+++ src/Variable/Klass/KlassException.class.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright   Copyright 2020, CitrusVariables. All Rights Reserved.
+ * @author      take64 <take64@citrus.tk>
+ * @license     http://www.citrus.tk/
+ */
+
+namespace Citrus\Variable\Klass;
+
+use Citrus\Variable\Strings;
+
+/**
+ * klass例外
+ */
+class KlassException
+{
+    /** @var string 型 */
+    protected $type;
+
+    /** @var string 例外コメントのフォーマット */
+    private $exception_comment_format = <<<FORMAT
+{{INDENT}} * @throws {{TYPE}}
+FORMAT;
+
+
+
+    /**
+     * constructor.
+     *
+     * @param string $type 型
+     */
+    public function __construct(string $type)
+    {
+        $this->type = $type;
+    }
+
+
+
+    /**
+     * 例外コメント文字列の返却
+     *
+     * @param KlassFormat $format フォーマット
+     * @return string
+     */
+    public function toExceptionCommentString(KlassFormat $format): string
+    {
+        // 置換パターン
+        $replace_patterns = [
+            '{{INDENT}}' => $format->indent,
+            '{{TYPE}}' => $this->type,
+        ];
+
+        // 置換して返却
+        return Strings::patternReplace($replace_patterns, $this->exception_comment_format);
+    }
+}
